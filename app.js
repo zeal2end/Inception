f91cedbb9a77411be07e80f9a1c1caf549bbc9f9
Vivek Kumar <vyvivekkumar@gmail.com>
2023-05-08 23:43:02 +0530

    database setup

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').parse()    
+}
+
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
 
+// Database 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+// Routes
 const indexRouter = require('./routes/index')
 
+// views
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
