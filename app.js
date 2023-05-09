ffb08f3f959bf18c5a6526e529b074d39683cafa
Vivek Kumar <vyvivekkumar@gmail.com>
2023-05-10 00:00:17 +0530

    database working and added gitignore

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').parse()    
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
 }
-
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
